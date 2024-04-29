e1ee0401961dbd8008c123a1041a28ed8b181536
EduardoElias-ifsp <166325459+EduardoElias-ifsp@users.noreply.github.com>
2024-04-29 19:51:58 -0300

    Update Program.cs

--- Lista02ex8/Lista02ex08/Program.cs
+++ Lista02ex8/Lista02ex08/Program.cs
@@ -23,23 +23,14 @@ namespace Lista02ex08
             Console.WriteLine("Digite o terceiro lado:");
             c = int.Parse(Console.ReadLine());
 
-            // Verifica se os valores digitados formam um triângulo
+
             if (a + b > c && a + c > b && b + c > a)
-            {
-                // Verifica se é um triângulo retângulo
                 if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b)
-                {
                     Console.WriteLine("Os lados formam um triângulo retângulo.");
-                }
                 else
-                {
                     Console.WriteLine("Os lados não formam um triângulo retângulo.");
-                }
-            }
             else
-            {
                 Console.WriteLine("Os valores digitados não formam um triângulo.");
-            }
         }
     }
 }
