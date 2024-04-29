1c0d893fe55ed76ab67aeb1fc3eda548a223eaa3
EduardoElias-ifsp <166325459+EduardoElias-ifsp@users.noreply.github.com>
2024-04-29 20:53:56 -0300

    Update Program.cs

--- Lista2ex3/Lista2ex3/Program.cs
+++ Lista2ex3/Lista2ex3/Program.cs
@@ -23,14 +23,31 @@ namespace Lista2ex3
             Console.WriteLine("Digite o terceiro valor: ");
             v3 = int.Parse(Console.ReadLine());
 
-            if (v1 > v2 && v1 > v3)
-                Console.WriteLine("O primeiro valor é o Maior = {0}", v1);
+            if (v1 == v2)
+                if (v1 == v3)
+                    Console.WriteLine("Todos valores são iguais = {0}", v1);
+                else
+                    if (v1 > v3)
+                    Console.WriteLine("O primeiro e segundo valor são os maiores = {0} ",v1);
+                else
+                    Console.WriteLine("O terceiro valor é o maior = {0}",v3);
             else
-            if (v2 > v1 && v2 > v3)
-                Console.WriteLine("O segundo valor é o Maior = {0}", v2);
-            else 
-                Console.WriteLine("O terceiro valor é o Maior = {0}", v3);
-
+                if (v1 == v3)
+                if (v1 > v3)
+                    Console.WriteLine("O primeiro e terceiro valor são os maiores = {0}", v1);
+                else
+                    Console.WriteLine("O segundo valor é o maior = {0}",v2);
+            else
+                    if (v2 == v3)
+                if (v2 > v1)
+                    Console.WriteLine("O segundo e terceiro valores são os maiores = {0}",v2);
+                else
+                    Console.WriteLine("O primeiro valor é o maior = {0}",v1);
+            else
+                if (v2 > v3)
+                Console.WriteLine("O segundo valor é o maior = {0}", v2);
+            else
+                Console.WriteLine("O terceiro valor é o maior = {0}",v3);
         }
     }
 }
